dc8f876610ea5de859879d70b981670edd99cd55
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 19:57:32 +0700

    js lab 140

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+// จงนับชื่อที่ปรากฏใน names
+
+let names = ['Jack', 'John', 'Bill', 'John', 'Rick', 'Bill', 'John'];
+
+const counts = {};
+names.forEach((item) => {
+  counts[item] = (counts[item] || 0) + 1;
+});
+console.log(counts)
+// expected result: {Jack: 1, John: 3, Bill: 2, Rick: 1}
